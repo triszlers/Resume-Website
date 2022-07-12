9ab8ef14bd53aeed8d21bd282dab1a910b21c791
triszlers <tristandallashunt@gmail.com>
2022-07-12 08:22:30 -0600

    openinnewtabfixed

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('a:not(a.subBtn)');
+    const anchors = document.querySelectorAll('a:not(a.subBtn):not(a.sub-item)');  //selects a (links) that are not of the listed classes (excluded from transitions)
 
-    setTimeout(() => {
-        transition_el.classList.remove('is-active');
+    setTimeout(() => {         //executes function once timer expires
+        transition_el.classList.remove('is-active');   //after 3 seconds, terminate transition and just show page
     }, 300)
 
     for (let i = 0; i < anchors.length; i++) {
